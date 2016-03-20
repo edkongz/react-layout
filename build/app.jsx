518b9956b4621cccf1342490e58fe519df4b418d
Ed <ed@kongdigital.net>
2016-03-20 12:00:36 +1100

    Switching over to new system

--- build/app.jsx
+++ build/app.jsx
@@ -7,7 +7,19 @@ import { FlexContainer, FlexColumn, FlexRow, FlexContent } from "react-layout"
 ReactDOM.render(
   <FlexContainer>
     <FlexColumn>
-      <FlexContent style={{ width: "200px"}}>Hello world</FlexContent>
+      <FlexContent style={{ background: "orange" }} flexItem={ false }>4</FlexContent>
+      <FlexRow style={{ border: "1px solid blue" }}>
+        <FlexContent style={{ background: "red" }} flexItem={ false }>1</FlexContent>
+        <FlexContent style={{ background: "purple", display: "flex" }}>
+          <FlexRow style={{ background: "green" }}>
+            <FlexColumn flexItem={false} style={{ background: "yellow" }}>
+              <FlexContent flexItem={false}>hello</FlexContent>
+            </FlexColumn>
+            <FlexContent flexItem={ false }>world</FlexContent>
+          </FlexRow>
+        </FlexContent>
+      </FlexRow>
     </FlexColumn>
+    Block
   </FlexContainer>
   ,document.getElementById("content"))
--- src/index.jsx
+++ src/index.jsx
@@ -2,45 +2,76 @@
 
 import React from "react"
 
-export const FlexContainer = props => {
-  const defaultStyle = {
-    padding: 0
-    ,width: "100vw"
-    ,height: "100vh"
-    ,boxSizing: "border-box"
+export class FlexContainer extends React.Component {
+
+  render() {
+    const defaultStyle = {
+      display: "flex"
+      ,width: "100vw"
+      ,height: "100vh"
+      ,boxSizing: "border-box"
+    }
+    const style = Object.assign(defaultStyle, this.props.style)
+    return <div style={ style }>
+      { this.props.children }
+    </div>
   }
-  const style = Object.assign(defaultStyle, props.style)
-  return <div style={ style }>{ props.children }</div>
 }
 
-export const FlexColumn = props => {
-  const defaultStyle = {
-    display: 'flex'
-    ,flexDirection: "column"
-    ,boxSizing: "border-box"
-    ,width: "100%"
-    ,height: "100%"
+export class FlexColumn extends React.Component {
+
+  render() {
+    const defaultStyle = {
+      display: 'flex'
+      ,flexDirection: "column"
+      ,boxSizing: "border-box"
+      ,width: this.props.width
+      ,height: this.props.height
+    }
+
+    let flexItem = { flex: this.props.flexItem || "1 0 auto" };
+    if (this.props.flexItem === false) flexItem = {}
+
+    const style = Object.assign(defaultStyle, flexItem, this.props.style)
+    return <div style={ style }>
+      { this.props.children }
+    </div>
   }
-  const style = Object.assign(defaultStyle, props.style)
-  return <div style={ style }>{ props.children }</div>
 }
 
-export const FlexRow = props => {
-  const defaultStyle = {
-    display: "flex"
-    ,boxSizing: "border-box"
+export class FlexRow extends React.Component {
+
+  render() {
+    const defaultStyle = {
+      display: "flex"
+      ,boxSizing: "border-box"
+      ,width: this.props.width
+      ,height: this.props.height
+    }
+    let flexItem = { flex: this.props.flexItem || "1 0 auto" };
+    if (this.props.flexItem === false) flexItem = {}
+
+    const style = Object.assign(defaultStyle, flexItem, this.props.style)
+    return <div style={ style }>{ this.props.children }</div>
   }
-  const style = Object.assign(defaultStyle, props.style)
-  return <div style={ style }>{ props.children }</div>
 }
 
+export class FlexContent extends React.Component {
 
-export const FlexContent = props => {
-  const defaultStyle = {
-    boxSizing: "border-box"
+  render() {
+    const defaultStyle = {
+      boxSizing: "border-box"
+      ,width: this.props.width
+      ,height: this.props.height
+    }
+
+    let flexItem = { flex: this.props.flexItem || "1 0 auto" };
+    if (this.props.flexItem === false) flexItem = {}
+    const style = Object.assign(defaultStyle, flexItem, this.props.style)
+    return <div style={ style }>{ this.props.children }</div>
   }
-  const centered = { margin: "auto" }
-
-  const style = Object.assign(defaultStyle, centered, props.style)
-  return <div style={ style }>{ props.children }</div>
 }
+
+export const FormRow = props => <FlexRow flexItem={ props.flexItem } style={{ padding: "0 10px" }}>{ props.children }</FlexRow>
+export const FormField = props => <FlexContent flexItem={ props.flexItem } style={{ padding: "0 10px" }}>{ props.children }</FlexContent>
+export const FormFooter = props => <FlexContent flexItem={ props.flexItem } style={{ padding: 10 }}>{ props.children }</FlexContent>
